b7a0a0e6bcec30cdc07bd9e3c6b7757fb4e36689
WilsonSanFullStack <harveysanch@gmail.com>
2024-03-31 02:15:35 -0500

    arreglo porcentaje funcion delete

--- src/routes/routesRegistros/rPorcentaje.js
+++ src/routes/routesRegistros/rPorcentaje.js
@@ -53,11 +53,11 @@ router.put("/:id", verifyJWT, async (req, res) => {
 router.delete("/delete/:id", verifyJWT, async (req, res) => {
   const { id } = req.params;
   try {
-    const deletePorcentaje = await deletePorcentaje(id);
-    if (!deletePorcentaje.error) {
-      return res.status(404).json(deletePorcentaje);
+    const deletesPorcentaje = await deletePorcentaje(id);
+    if (!deletesPorcentaje.error) {
+      return res.status(404).json(deletesPorcentaje);
     }
-    return res.status(200).json(deletePorcentaje);
+    return res.status(200).json(deletesPorcentaje);
   } catch (error) {
     return res.status(500).send(error.message);
   }
