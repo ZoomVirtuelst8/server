9c84553d9fb531c35217b58f93d0c408107422f0
WilsonSanFullStack <harveysanch@gmail.com>
2024-04-05 06:51:40 -0500

    if

--- src/routes/rUserName.js
+++ src/routes/rUserName.js
@@ -16,9 +16,9 @@ router.post("/", verifyJWT, async (req, res) => {
   try {
     const nUserName = await postUserName(userNames);
     const newUserNames = nUserName[0]
-    if (newUserNames?.Error) {
-      return res.status(404).json(newUserNames);
-    }
+    // if (newUserNames?.Error) {
+    //   return res.status(404).json(newUserNames);
+    // }
     return res.status(200).json(newUserNames);
   } catch (error) {
     return res.status(500).send(error.message);
